52b7a5175fd8fe34ececd0116ea532bbc977b07a
Santos <realsantosm@gmail.com>
2022-10-31 01:26:17 +0530

    main files updated

---
+++ main.js
@@ -0,0 +1,8 @@
+const mainDiv = document.querySelector("#main-box");
+const siteName = document.querySelector("#site");
+const saveUrl = document.querySelector("#siteUrl");
+
+const addBtn = document.querySelector("#addBtn");
+const removeBtn = document.querySelector("#removeBtn");
+
+console.table(siteName + " " + saveUrl + " " + addBtn + " " + removeBtn);
